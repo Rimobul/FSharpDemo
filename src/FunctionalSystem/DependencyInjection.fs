ccbfcd813eba7b9a1bca42964724ea05b7b4f1b5
Ľubomír Sokolovský <lubo.sokolovsky@gmail.com>
2018-06-13 08:39:24 +0200

    DI

---
+++ src/FunctionalSystem/DependencyInjection.fs
@@ -0,0 +1,24 @@
+namespace FunctionalSystem
+
+module DependencyInjection = 
+    type Application = Application
+    type Ids = Ids
+    type Deposit = Deposit
+    type CheckingAccount = CheckingAccount
+
+    let validateApplication (application: Application) =
+        true
+
+    let openCheckingAccount
+        validateApplication
+        validateIds
+        validateDeposit
+        (application: Application)
+        (ids: Ids)
+        (deposit: Deposit) = 
+
+        if not (validateApplication application) then None
+        elif not (validateIds ids) then None
+        elif not (validateDeposit deposit) then None
+        else Some CheckingAccount
+    
---
+++ src/FunctionalSystem/Library.fs
@@ -0,0 +1,5 @@
+namespace FunctionalSystem
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
